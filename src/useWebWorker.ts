5fa6acf1d8867588b6b246c678138c1229d0dad1
Alexander Snov <snov.contact@gmail.com>
2021-10-10 01:03:03 +0700

    Track ready state in `useWebWorker` hook

--- src/useWebWorker.ts
+++ src/useWebWorker.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useMemo, useCallback } from 'react';
+import { useEffect, useRef, useMemo, useState } from 'react';
 
 import { DuplexConnector, EventSystemParams } from '@osnova/events';
 import { RequestEvent, UnwrapRequestEvent } from '@osnova/events/EventRequest';
@@ -22,12 +22,13 @@ export function useWebWorker<
 >(
   initializer: DuplexConnectorInitializer<OutReqEvents, InReqEvents, OutResponseEventMap, InResponseEventMap>,
   params: EventSystemParams<OutReqEvents, InReqEvents, OutResponseEventMap, InResponseEventMap>
-): { useDataEvent: UseDataEventHook } & NullableSystemConnector<
+): { isReady: boolean; useDataEvent: UseDataEventHook } & NullableSystemConnector<
   OutReqEvents,
   InReqEvents,
   OutResponseEventMap,
   InResponseEventMap
 > {
+  const [isReady, setIsReady] = useState(false);
   const request = useRef<RequestType<OutReqEvents, InResponseEventMap> | null>(null);
   const on = useRef<OnType<InReqEvents, InResponseEventMap> | null>(null);
 
@@ -47,6 +48,8 @@ export function useWebWorker<
       on.current = connector.on.bind(connector);
       sourceReadyResolve.current({ request: request.current, on: on.current });
 
+      setIsReady(true);
+
       if (params.onBoot) {
         params.onBoot({ request: request.current, on: on.current });
       }
@@ -57,7 +60,7 @@ export function useWebWorker<
 
   const systemInterface = { request: request.current, on: on.current };
 
-  return { ...systemInterface, useDataEvent };
+  return { ...systemInterface, useDataEvent, isReady };
 }
 
 export type UsedWebWorkerConnector = ReturnType<typeof useWebWorker>;
