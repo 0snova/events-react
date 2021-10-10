b4d1cf63c0889ebbb600d448f562250721d7920a
Alexander Snov <snov.contact@gmail.com>
2021-10-10 15:38:59 +0700

    Add feature: timeout for request

--- src/useConnectorInitializer.ts
+++ src/useConnectorInitializer.ts
@@ -14,6 +14,15 @@ export type DuplexConnectorInitializer<
   InResponseEventMap extends AnyResponseEventMap
 > = () => Promise<{ connector: DuplexConnector<OutReqEvents, InReqEvents, OutResponseEventMap, InResponseEventMap> }>;
 
+export interface ConnectorInitializerParams<
+  OutReqEvents extends RequestEvent,
+  InReqEvents extends RequestEvent,
+  OutResponseEventMap extends AnyResponseEventMap,
+  InResponseEventMap extends AnyResponseEventMap
+> extends EventSystemParams<OutReqEvents, InReqEvents, OutResponseEventMap, InResponseEventMap> {
+  requestTimeout?: number;
+}
+
 export function useConnectorInitializer<
   OutReqEvents extends RequestEvent,
   InReqEvents extends RequestEvent,
@@ -21,7 +30,7 @@ export function useConnectorInitializer<
   InResponseEventMap extends AnyResponseEventMap
 >(
   initializer: DuplexConnectorInitializer<OutReqEvents, InReqEvents, OutResponseEventMap, InResponseEventMap>,
-  params: EventSystemParams<OutReqEvents, InReqEvents, OutResponseEventMap, InResponseEventMap>
+  params: ConnectorInitializerParams<OutReqEvents, InReqEvents, OutResponseEventMap, InResponseEventMap>
 ): NullableSystemConnector<OutReqEvents, InReqEvents, OutResponseEventMap, InResponseEventMap> {
   const [isReady, setIsReady] = useState(false);
   const request = useRef<RequestType<OutReqEvents, InResponseEventMap> | null>(null);
@@ -59,7 +68,24 @@ export function useConnectorInitializer<
 
   const systemInterface = { request: request.current, on: on.current };
 
-  return { ...systemInterface, useDataEvent, isReady, onReady: sourceReadyPromise };
+  const onReady = useMemo(async () => {
+    const requestTimeout = params.requestTimeout ?? 1000;
+    const { request, on } = await sourceReadyPromise;
+
+    const requestWithTimeout: RequestType<OutReqEvents, InResponseEventMap> = (event) =>
+      Promise.race([
+        request(event),
+        new Promise((_, reject) =>
+          setTimeout(() => {
+            reject({ error: `Timeout` });
+          }, requestTimeout)
+        ),
+      ]);
+
+    return { request: requestWithTimeout, on };
+  }, [params.requestTimeout]);
+
+  return { ...systemInterface, useDataEvent, isReady, onReady };
 }
 
 export type UseConnectorInitializer = ReturnType<typeof useConnectorInitializer>;
